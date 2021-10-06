cc155d42d088dfe4da53a89dd70b4d92d1571c3c
Edilberto Vazquez Luna <edi_vazquez12@hotmail.com>
2021-10-05 20:31:34 -0500

    create a server and some routes

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Hello")
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, my server in express');
+});
+
+app.get('/new-route', (req, res) => {
+  res.send('Hello, i am a new rute');
+});
+
+app.get('/products', (req, res) => {
+  res.json({
+    name: 'Product 1',
+    price: 305,
+  });
+});
+
+app.listen(port, () => {
+  console.log('Mi port ' + port);
+});
