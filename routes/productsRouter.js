6459dd1ead9f525de9983f3b6da9e4a02ba6f210
Edilberto Vazquez Luna <edi_vazquez12@hotmail.com>
2021-10-06 22:51:57 -0500

    async and errors

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -5,8 +5,8 @@ const ProductsService = require('../services/productService.js');
 const service = new ProductsService();
 
 // method to get products
-router.get('/', (req, res) => {
-  const products = service.find();
+router.get('/', async (req, res) => {
+  const products = await service.find();
   res.json(products);
 });
 
@@ -15,34 +15,46 @@ router.get('/filter', (req, res) => {
   res.send('i am a filter');
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   const { id } = req.params;
-  const product = service.findOne(id);
+  const product = await service.findOne(id);
   res.json(product);
 });
 
 // post method
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   const body = req.body;
-  const newProduct = service.create(body);
+  const newProduct = await service.create(body);
   res.status(201).json({
     data: newProduct,
   });
 });
 
 // patch method
-router.patch('/:id', (req, res) => {
-  const { id } = req.params;
-  const body = req.body;
-  const product = service.update(id, body);
-  res.json(product);
+router.patch('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const body = req.body;
+    const product = await service.update(id, body);
+    res.json(product);
+  } catch (error) {
+    res.status(404).json({
+      message: error.message,
+    });
+  }
 });
 
 // delete method
-router.delete('/:id', (req, res) => {
-  const { id } = req.params;
-  const rta = service.delete(id);
-  res.json(rta);
+router.delete('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const rta = await service.delete(id);
+    res.json(rta);
+  } catch (error) {
+    res.status(404).json({
+      message: error.message,
+    });
+  }
 });
 
 module.exports = router;
