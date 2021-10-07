f8f80e34bec8f8a6bdeb96ea21472315b2b29d1e
Edilberto Vazquez Luna <edi_vazquez12@hotmail.com>
2021-10-06 21:42:39 -0500

    service created

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -1,21 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const faker = require('faker');
+const ProductsService = require('../services/productService.js');
+
+const service = new ProductsService();
 
 // method to get products
 router.get('/', (req, res) => {
-  const { size } = req.query;
-  const limit = size || 10;
-  const products = [];
-
-  for (let index = 0; index < limit; index++) {
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.imageUrl(),
-    });
-  }
-
+  const products = service.find();
   res.json(products);
 });
 
@@ -26,17 +17,8 @@ router.get('/filter', (req, res) => {
 
 router.get('/:id', (req, res) => {
   const { id } = req.params;
-  if (id === '999') {
-    res.status(404).json({
-      message: 'not found',
-    });
-  } else {
-    res.status(200).json({
-      id,
-      name: 'Product 5',
-      price: 400,
-    });
-  }
+  const product = service.findOne(id);
+  res.json(product);
 });
 
 // post method
