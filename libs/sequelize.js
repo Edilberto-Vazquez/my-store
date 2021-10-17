67f56fa7d634164904c61d18c5a21e21958bd445
Edilberto Vazquez Luna <edi_vazquez12@hotmail.com>
2021-10-17 18:47:16 -0500

    install cli

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -1,11 +1,7 @@
 const { Sequelize } = require('sequelize');
 
-const { config } = require('../config/config');
-const setupModels = require('../db/models');
-
-// const USER = encodeURIComponent(config.dbUser);
-// const PASSWORD = encodeURIComponent(config.dbPassword);
-// const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const { config } = require('./../config/config');
+const setupModels = require('./../db/models');
 
 const options = {
   dialect: 'postgres',
@@ -23,6 +19,5 @@ if (config.isProd) {
 const sequelize = new Sequelize(config.dbUrl, options);
 
 setupModels(sequelize);
-// sequelize.sync();
 
 module.exports = sequelize;
